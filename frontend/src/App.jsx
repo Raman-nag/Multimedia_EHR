39aaa1736ac15c516ddebcc5110df03aaeca5439
Raman Nag <raman.nag.ngit@gmail.com>
2025-10-26 08:01:18 +0530

    updated app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,77 +1,8 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from './contexts/ThemeContext'
-import { AuthProvider } from './contexts/AuthContext'
-import { Web3Provider } from './contexts/Web3Context'
-import { IPFSProvider } from './contexts/IPFSContext'
-
-// Pages
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import PatientDashboard from './pages/PatientDashboard'
-import DoctorDashboard from './pages/DoctorDashboard'
-import HospitalDashboard from './pages/HospitalDashboard'
-import StyleTest from './pages/StyleTest'
-import NotFound from './pages/NotFound'
-
-// Layouts
-import AuthLayout from './components/layout/AuthLayout'
-import DashboardLayout from './components/layout/DashboardLayout'
-
-// Loading component
-import Loader from './components/common/Loader'
+import AppRouter from './routes'
 
 function App() {
-  return (
-    <ThemeProvider>
-      <Web3Provider>
-        <IPFSProvider>
-          <AuthProvider>
-            <div className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-300">
-              <Routes>
-                {/* Public routes */}
-                <Route path="/" element={<Home />} />
-                <Route path="/style-test" element={<StyleTest />} />
-                
-                {/* Auth routes */}
-                <Route path="/login" element={
-                  <AuthLayout>
-                    <Login />
-                  </AuthLayout>
-                } />
-                <Route path="/register" element={
-                  <AuthLayout>
-                    <Register />
-                  </AuthLayout>
-                } />
-                
-                {/* Dashboard routes */}
-                <Route path="/patient-dashboard" element={
-                  <DashboardLayout>
-                    <PatientDashboard />
-                  </DashboardLayout>
-                } />
-                <Route path="/doctor-dashboard" element={
-                  <DashboardLayout>
-                    <DoctorDashboard />
-                  </DashboardLayout>
-                } />
-                <Route path="/hospital-dashboard" element={
-                  <DashboardLayout>
-                    <HospitalDashboard />
-                  </DashboardLayout>
-                } />
-                
-                {/* 404 route */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </div>
-          </AuthProvider>
-        </IPFSProvider>
-      </Web3Provider>
-    </ThemeProvider>
-  )
+  return <AppRouter />
 }
 
 export default App
