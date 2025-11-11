eeb55e17b6eddcbc7d2995db328804e2b4b6d242
Raman Nag <raman.nag.ngit@gmail.com>
2025-11-11 18:19:07 +0530

    routing problem solved

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -12,10 +12,13 @@ import NotFound from "./pages/NotFound";
 import AddDoctorPage from "./pages/hospital/AddDoctorPage";
 import PatientsPage from "./pages/hospital/PatientsPage";
 import AnalyticsPage from "./pages/hospital/AnalyticsPage";
+import DoctorsList from "./pages/hospital/DoctorsList";
 import CreateRecord from "./components/doctor/CreateRecord";
 import MyPatients from "./components/doctor/MyPatients";
 import PrescriptionForm from "./components/doctor/PrescriptionForm";
 import ViewPatientHistory from "./components/doctor/ViewPatientHistory";
+import DoctorPrescriptions from "./components/doctor/DoctorPrescriptions";
+import GetPatientDetails from "./components/doctor/GetPatientDetails";
 import MedicalHistory from "./components/patient/MedicalHistory";
 import Prescriptions from "./components/patient/Prescriptions";
 import GrantAccess from "./components/patient/GrantAccess";
@@ -30,6 +33,7 @@ export default function AppRouter() {
       
       {/* Hospital Routes */}
       <Route path="/hospital/dashboard" element={<HospitalDashboard />} />
+      <Route path="/hospital/doctors" element={<DoctorsList />} />
       <Route path="/hospital/add-doctor" element={<AddDoctorPage />} />
       <Route path="/hospital/patients" element={<PatientsPage />} />
       <Route path="/hospital/analytics" element={<AnalyticsPage />} />
@@ -38,7 +42,9 @@ export default function AppRouter() {
       {/* Doctor subpages */}
       <Route path="/doctor/create-record" element={<CreateRecord />} />
       <Route path="/doctor/my-patients" element={<MyPatients />} />
+      <Route path="/doctor/get-patient-details" element={<GetPatientDetails />} />
       <Route path="/doctor/write-prescription" element={<PrescriptionForm />} />
+      <Route path="/doctor/prescriptions" element={<DoctorPrescriptions />} />
       <Route path="/doctor/patient-history/:patientId" element={<ViewPatientHistory />} />
       <Route path="/patient/dashboard" element={<PatientDashboard />} />
       {/* Patient subpages */}
